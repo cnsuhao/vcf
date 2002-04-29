e3e3a4d3f1f77f1afb6c2fe71cfe51a374d24058
nobody <nobody@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2002-04-29 03:30:05 +0000

    This commit was manufactured by cvs2svn to create branch 'lucky'.

---
+++ branches/lucky/vcf/build/vc60/add-ins/VCFConsoleWiz/Template/consoleMain.cpp
@@ -0,0 +1,25 @@
+////$$root$$.cpp
+
+#include "FoundationKit.h"
+
+using namespace VCF;
+
+int main( int argc, char** argv ){
+
+	FoundationKit::init();
+
+	
+
+	FoundationKit::terminate();
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
