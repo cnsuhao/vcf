4a0635ff26b205da9e4fd11ba19960d8e274c41e
nobody <nobody@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2003-08-09 21:01:55 +0000

    This commit was manufactured by cvs2svn to create tag 'release-0-6-2'.

---
+++ tags/release-0-6-2/vcf/build/vc60/add-ins/VCFAppWiz/template/main.cpp
@@ -0,0 +1,49 @@
+//$$root$$.cpp
+
+
+#include "ApplicationKit.h"
+
+
+using namespace VCF;
+
+
+class $$Root$$Window : public Window {
+public:
+	$$Root$$Window() {
+		setCaption( "$$Root$$" );
+		setVisible( true );
+	}
+
+	virtual ~$$Root$$Window(){};
+
+};
+
+
+
+
+class $$Root$$Application : public Application {
+public:
+
+	virtual bool initRunningApplication(){
+		bool result = Application::initRunningApplication();
+		
+		Window* mainWindow = new $$Root$$Window();
+		setMainWindow(mainWindow);
+		mainWindow->setBounds( &Rect( 100.0, 100.0, 500.0, 500.0 ) );
+		
+		return result;
+	}
+
+};
+
+
+int main(int argc, char *argv[])
+{
+	$$Root$$Application app;
+
+	Application::main( argc, argv );
+	
+	return 0;
+}
+
+
