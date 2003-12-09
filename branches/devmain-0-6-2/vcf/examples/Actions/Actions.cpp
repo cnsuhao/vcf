f84d874bcaa9e4a540bfc637015c9a1dbf5d7987
ddiego <ddiego@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2003-12-09 04:19:21 +0000

    mostly finished the Actions example

--- branches/devmain-0-6-2/vcf/examples/Actions/Actions.cpp
+++ branches/devmain-0-6-2/vcf/examples/Actions/Actions.cpp
@@ -159,6 +159,86 @@ public:
 		add( sideBar_, AlignLeft );
 
 
+		hammerBar_ = new Panel();
+		hammerBar_->setColor( Color::getColor( "gray105" ) );
+		hammerBar_->setHeight( 80 );
+		Label* label = new Label();
+		label->getFont()->setPointSize( 20 );
+		label->setCaption( "Hammer Time!" );
+		hammerBar_->add( label, AlignClient );
+		add( hammerBar_, AlignBottom );
+
+
+
+		//actions
+
+		Action* fileNewAction = new Action();
+		addComponent( fileNewAction );
+		fileNewAction->addTarget(fileNew);
+		fileNewAction->addTarget(newTBitem);
+		fileNewAction->Performed += 
+			new GenericEventHandler<ActionsWindow>(this, &ActionsWindow::onFileNew, "ActionsWindow::onFileNew" );
+
+		Action* fileOpenAction = new Action();
+		addComponent( fileOpenAction );
+		fileOpenAction->addTarget(fileOpen);
+		fileOpenAction->addTarget(openTBitem);
+		fileOpenAction->Performed += 
+			new GenericEventHandler<ActionsWindow>(this, &ActionsWindow::onFileOpen, "ActionsWindow::onFileOpen" );
+
+
+		Action* fileSaveAction = new Action();
+		addComponent( fileSaveAction );
+		fileSaveAction->addTarget(fileSave);
+		fileSaveAction->addTarget(saveTBitem);
+		
+		fileSaveAction->Performed += 
+			new GenericEventHandler<ActionsWindow>(this, &ActionsWindow::onFileSave, "ActionsWindow::onFileSave" );
+
+		fileSaveAction->Update += 
+			new EventHandlerInstance<ActionsWindow,ActionEvent>(this, &ActionsWindow::onUpdateFileSave, "ActionsWindow::onUpdateFileSave" );
+
+		
+
+		Action* fileSaveAllAction = new Action();
+		addComponent( fileSaveAllAction );
+		fileSaveAllAction->addTarget(fileSaveAll);
+		fileSaveAllAction->addTarget(saveAllTBitem);
+		fileSaveAllAction->Performed += 
+			new GenericEventHandler<ActionsWindow>(this, &ActionsWindow::onFileSaveAll, "ActionsWindow::onFileSaveAll" );
+		fileSaveAllAction->Update += 
+			new EventHandlerInstance<ActionsWindow,ActionEvent>(this, &ActionsWindow::onUpdateFileSaveAll, "ActionsWindow::onUpdateFileSaveAll" );
+
+
+		Action* editCopyAction = new Action();
+		addComponent( editCopyAction );
+		editCopyAction->addTarget(editCopy);
+		editCopyAction->addTarget(copyTBitem);
+		editCopyAction->Performed += 
+			new GenericEventHandler<ActionsWindow>(this, &ActionsWindow::onEditCopy, "ActionsWindow::onEditCopy" );
+
+		Action* editPasteAction = new Action();
+		addComponent( editPasteAction );
+		editPasteAction->addTarget(editPaste);
+		editPasteAction->addTarget(pasteTBitem);
+		
+		editPasteAction->Performed += 
+			new GenericEventHandler<ActionsWindow>(this, &ActionsWindow::onEditPaste, "ActionsWindow::onEditPaste" );
+		editPasteAction->Update += 
+			new EventHandlerInstance<ActionsWindow,ActionEvent>(this, &ActionsWindow::onUpdateEditPaste, "ActionsWindow::onUpdateEditPaste" );
+
+		Action* editUndoAction = new Action();
+		addComponent( editUndoAction );
+		editUndoAction->addTarget(editUndo);
+		editUndoAction->addTarget(undoTBitem);
+		
+		editUndoAction->Performed += 
+			new GenericEventHandler<ActionsWindow>(this, &ActionsWindow::onEditUndo, "ActionsWindow::onEditUndo" );
+		editUndoAction->Update += 
+			new EventHandlerInstance<ActionsWindow,ActionEvent>(this, &ActionsWindow::onUpdateEditUndo, "ActionsWindow::onUpdateEditUndo" );
+
+
+
 		Action* viewSidebarAction = new Action();
 		addComponent( viewSidebarAction );
 
@@ -171,11 +251,85 @@ public:
 		viewSidebarAction->Update += 
 			new EventHandlerInstance<ActionsWindow,ActionEvent>(this, &ActionsWindow::onUpdateViewSidebar, "ActionsWindow::onUpdateViewSidebar" );
 
+
+		Action* viewHammerBarAction = new Action();
+		addComponent( viewHammerBarAction );
+
+		viewHammerBarAction->addTarget( viewHammer );
+		viewHammerBarAction->addTarget( hammerTBitem );
+
+		viewHammerBarAction->Performed += 
+			new GenericEventHandler<ActionsWindow>(this, &ActionsWindow::onViewHammerBar, "ActionsWindow::onViewHammerBar" );
+
+		viewHammerBarAction->Update += 
+			new EventHandlerInstance<ActionsWindow,ActionEvent>(this, &ActionsWindow::onUpdateViewHammerBar, "ActionsWindow::onUpdateViewHammerBar" );
+
+
+		canSave_ = false;
+		canPaste_ = false;
 	}
 
 	virtual ~ActionsWindow(){};
 
 	Panel* sideBar_;
+	Panel* hammerBar_;
+
+	bool canSave_;
+	bool canPaste_;
+
+
+	void onFileNew( Event* e ) {
+		canSave_ = true;
+		Dialog::showMessage( "File New!" );
+	}
+
+	void onFileOpen( Event* e ) {
+		canSave_ = true;
+		Dialog::showMessage( "File Open!" );
+	}
+
+	void onFileSave( Event* e ) {
+		canSave_ = false;
+		Dialog::showMessage( "File Save!" );
+	}
+
+	void onUpdateFileSave( ActionEvent* e ) {
+		e->setEnabled( canSave_ );
+	}
+
+	void onFileSaveAll( Event* e ) {
+		canSave_ = false;
+		Dialog::showMessage( "File Save All!" );
+	}
+
+	void onUpdateFileSaveAll( ActionEvent* e ) {
+		e->setEnabled( canSave_ );
+	}
+
+	void onEditCopy( Event* e ) {
+		canPaste_ = true;
+		Dialog::showMessage( "Edit Copy!" );
+	}
+	
+
+	void onEditPaste( Event* e ) {
+		canPaste_ = false;
+		Dialog::showMessage( "Edit Paste!" );
+	}
+
+	void onUpdateEditPaste( ActionEvent* e ) {
+		e->setEnabled( canPaste_ );
+	}
+
+	void onEditUndo( Event* e ) {
+		Dialog::showMessage( "Edit Undo!" );
+	}
+
+	void onUpdateEditUndo( ActionEvent* e ) {
+		
+	}
+
+
 
 	void onViewSidebar( Event* e ) {
 		sideBar_->setVisible( !sideBar_->getVisible() );
@@ -189,6 +343,25 @@ public:
 			e->setState( e->getState() & ~ToolbarItem::tisPressed );
 		}
 		e->setChecked( sideBar_->getVisible() );
+	}
+
+	void onViewHammerBar( Event* e ) {
+		hammerBar_->setVisible( !hammerBar_->getVisible() );
+	}
+
+	void onUpdateViewHammerBar( ActionEvent* e ) {
+		if ( hammerBar_->getVisible() ) {
+			e->setState( e->getState() | ToolbarItem::tisPressed );
+		}
+		else {
+			e->setState( e->getState() & ~ToolbarItem::tisPressed );
+		}
+		e->setChecked( hammerBar_->getVisible() );
+	}
+
+
+	void onViewWindows( Event* e ) {
+
 	}
 
 };
