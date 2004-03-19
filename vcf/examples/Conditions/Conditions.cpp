9e5868dd5511de1c646c378344cf46de9b871042
ddiego <ddiego@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2004-03-19 04:18:53 +0000

    added more code to condition example, fixed some minor stuff in the table control

--- vcf/examples/Conditions/Conditions.cpp
+++ vcf/examples/Conditions/Conditions.cpp
@@ -6,31 +6,40 @@ using namespace VCF;
 
 
 static Mutex* theMutex = NULL;
+static Mutex* printMutex = NULL;
 static Condition* theCondition = NULL;
 static producerDone = false;
+
+
+void println( const String& s ) 
+{
+	Lock l(*printMutex);
+	System::println( s );
+}
+
 
 class Reporter : public Runnable {
 public:
 	virtual bool run() {
 		int count = 0;
-		System::println( "Reporter %p starting...", this );
+		println( StringUtils::format( "Reporter %p starting...", this ) );
 
 		while ( count < 10 && !producerDone ) {
 			Lock l(*theMutex);
 			
 			theCondition->wait();
 
-			System::println( "Reporter: Done waiting report %d, reporting from %p:\n\tNews Flash! Yada yada yada!", count + 1, this );
+			println( StringUtils::format( "Reporter: Done waiting report %d, reporting from %p:\n\tNews Flash! Yada yada yada!", count + 1, this ) );
 			count ++;
 		}
 
-		System::println( "Reporter done!" );
+		println( StringUtils::format( "Reporter done!" ) );
 
 		return true;
 	}
 
 	virtual void stop() {
-		System::println( "The Reporter has left the building!" );
+		println( StringUtils::format( "The Reporter has left the building!" ) );
 	}
 };
 
@@ -42,23 +51,22 @@ public:
 		while ( count < 10 ) {
 			Lock l(*theMutex);
 
-			System::println("Producer chomping %d vitamins...\n\tchomp, chomp, chomp...", count );
+			println( StringUtils::format("Producer chomping %d vitamins...\n\tchomp, chomp, chomp...", count ));
 			System::sleep( 300 );
-			System::println( "Yawn...Burp!" );
+			println( StringUtils::format( "Yawn...Burp!" ));
 			System::sleep( 100 );
 			System::println( "...Fart!" );
 
-
-
+			
 			theCondition->broadcast();
 
-			System::println( "Done chomping!" );
+			println( StringUtils::format( "Done chomping!" ));
 
 
 			count++;
 		}
 
-		System::println( "Producer done!" );
+		println( StringUtils::format( "Producer done!" ));
 		producerDone = true;
 		theCondition->broadcast();
 
@@ -72,12 +80,9 @@ public:
 
 
 
-
-int main( int argc, char** argv ){
-
-	FoundationKit::init( argc, argv );
-
+void example1() {
 	theMutex = new Mutex();
+	printMutex = new Mutex();
 
 	theCondition = new Condition( theMutex );
 
@@ -102,9 +107,141 @@ int main( int argc, char** argv ){
 
 	theMutex->free();
 	theCondition->free();
+}
+
+
+
+
+class bounded_buffer {
+public:
+    
+    bounded_buffer(int n) : begin(0), end(0), buffered(0), circular_buf(n) { 
+		buffer_not_full = new Condition( &monitor );
+		buffer_not_empty = new Condition( &monitor );
+	}
+
+	~bounded_buffer() {
+		buffer_not_full->free();
+		buffer_not_empty->free();
+	}
+
+    void send (int m) {
+        Lock lk(monitor);
+
+        while (buffered == circular_buf.size()) {
+            buffer_not_full->wait();
+		}
+
+        circular_buf[end] = m;
+
+        end = (end+1) % circular_buf.size();
+
+        buffered ++;
+
+        buffer_not_empty->signal();
+    }
+
+    int receive() {
+        Lock lk(monitor);
+        while (buffered == 0) {
+            buffer_not_empty->wait();
+		}
+
+        int i = circular_buf[begin];
+        
+		begin = (begin+1) % circular_buf.size();
+
+        buffered --;
+
+        buffer_not_full->signal();
+
+        return i;
+    }
+
+private:
+    int begin, end, buffered;
+
+    std::vector<int> circular_buf;
+
+    Condition* buffer_not_full;
+	Condition* buffer_not_empty;
+
+    Mutex monitor;
+};
+
+
+
+bounded_buffer* buffer;
+int threadsDone = 2;
+
+class Sender : public Thread {
+public:
+	
+	virtual bool run() {
+		int n = 0;
+		while (n < 100) {
+			buffer->send(n);
+			System::println( "sent: %d", n );
+			++n;
+		}
+		buffer->send(-1);
+
+		threadsDone --;
+		theCondition->signal();
+
+		return true;
+	}
+};
+
+class Receiver : public Thread {
+public:
+	
+	virtual bool run() {
+		int n;
+		do {
+			n = buffer->receive();
+			System::println( "received: %d", n );
+		} while (n != -1); // -1 indicates end of buffer
+
+		threadsDone --;
+		theCondition->signal();
+		return true;
+	}
+};
+
+
+void example2() 
+{
+	theMutex = new Mutex();
+	theCondition = new Condition( theMutex );
+
+	buffer = new bounded_buffer(10);
+	Thread* send = new Sender();
+	Thread* recv = new Receiver();
+
+	send->start();
+	recv->start();
+
+	while ( threadsDone > 0 ) {
+		theCondition->wait();
+	}
+
+	System::println( "example2 completed!" );
+}
+
+
+int main( int argc, char** argv ){
+
+	FoundationKit::init( argc, argv );
+
+	example1();
+
+	example2();
+
 
 	FoundationKit::terminate();
 	return 0;
 }
+
 
 
