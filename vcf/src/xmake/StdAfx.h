c195d1674411df6d6f7b8a8e1dcd5e449768ca10
ddiego <ddiego@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2004-04-28 04:07:36 +0000

    migration towards new directory structure

---
+++ vcf/src/xmake/StdAfx.h
@@ -0,0 +1,28 @@
+
+#if !defined(AFX_STDAFX_H__9B00A488_387C_4246_88C3_FF34CF33B851__INCLUDED_)
+#define AFX_STDAFX_H__9B00A488_387C_4246_88C3_FF34CF33B851__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+
+#ifdef WIN32
+	#pragma warning (disable : 4786 )
+#endif
+
+#include <stdio.h>
+
+
+#ifdef WIN32
+	#include <windows.h>
+    #include <io.h>
+#endif 
+
+
+#include "xmake.h"
+
+
+#endif // !defined(AFX_STDAFX_H__9B00A488_387C_4246_88C3_FF34CF33B851__INCLUDED_)
+
+
