b163c627605b97829cf69b42e13fc4f05c73ac15
kiklop74 <kiklop74@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2004-10-05 02:48:22 +0000

    Added needed changes that will enable Borland compiler to compile RTTI for ApplicationKit

--- vcf/src/vcf/ApplicationKit/TableModel.h
+++ vcf/src/vcf/ApplicationKit/TableModel.h
@@ -80,48 +80,6 @@ public:
 
 	virtual ~TableModel(){};
 
-	/**
-	@delegate TableCellAdded
-	@event TableModelEvent
-	*/
-	DELEGATE(TableCellAdded)
-
-	/**
-	@delegate TableCellDeleted
-	@event TableModelEvent
-	*/
-	DELEGATE(TableCellDeleted)
-
-	/**
-	@delegate TableCellsSelected
-	@event TableModelEvent
-	*/
-	DELEGATE(TableCellsSelected)
-
-	/**
-	@delegate TableRowAdded
-	@event TableModelEvent
-	*/
-	DELEGATE(TableRowsAdded)
-
-	/**
-	@delegate TableRowDeleted
-	@event TableModelEvent
-	*/
-	DELEGATE(TableRowsDeleted)
-
-	/**
-	@delegate TableColumnAdded
-	@event TableModelEvent
-	*/
-	DELEGATE(TableColumnsAdded)
-
-	/**
-	@delegate TableColumnDeleted
-	@event TableModelEvent
-	*/
-	DELEGATE(TableColumnsDeleted)
-
     virtual void addRow() = 0;
 
 	virtual void insertRow( const uint32& afterRow ) = 0;
@@ -189,6 +147,9 @@ public:
 /**
 *CVS Log info
 *$Log$
+*Revision 1.2.2.2  2004/10/05 02:48:22  kiklop74
+*Added needed changes that will enable Borland compiler to compile RTTI for ApplicationKit
+*
 *Revision 1.2.2.1  2004/09/21 23:41:24  ddiego
 *made some big changes to how the base list, tree, text, table, and tab models are laid out. They are not just plain interfaces. The actual
 *concrete implementations of them now derive from BOTH Model and the specific
