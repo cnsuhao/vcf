752c70f9f4a846748ef41bf0cb7134fae95e2716
kiklop74 <kiklop74@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2004-10-05 03:15:23 +0000

    Additional changes in tree model

--- branches/devmain-0-6-6/vcf/src/vcf/ApplicationKit/DefaultTreeModel.cpp
+++ branches/devmain-0-6-6/vcf/src/vcf/ApplicationKit/DefaultTreeModel.cpp
@@ -53,10 +53,63 @@ void DefaultTreeModel::onItemDeleted( ItemEvent* event )
 
 }
 
+void DefaultTreeModel::insertNodeItem(TreeItem * node, TreeItem * nodeToInsertAfter)
+{
+	TreeItem* parent = nodeToInsertAfter->getParent();
+	if ( NULL != parent ) {
+		parent->insertChild( nodeToInsertAfter->getIndex(), node );
+		TreeModelEvent event(dynamic_cast<Object*>(this), node, TreeModel::TREEITEM_ADDED );
+		NodeAdded.fireEvent( &event );
+	}
+	else {
+		//bad call throw exception
+	}
+}
+
+void DefaultTreeModel::deleteNodeItem(TreeItem * nodeToDelete)
+{
+	TreeItem* parent = nodeToDelete->getParent();
+
+	TreeModelEvent event(dynamic_cast<Object*>(this), nodeToDelete, TreeModel::TREEITEM_DELETED );
+	NodeDeleted.fireEvent( &event );
+
+	if ( NULL != parent ){
+		parent->deleteChild( nodeToDelete );
+	}
+	else {
+		std::vector<TreeItem*>::iterator found = std::find( rootNodes_.begin(), rootNodes_.end(), nodeToDelete );
+		if ( found != rootNodes_.end() ) {
+			TreeItem* item = *found;
+			rootNodes_.erase( found );			
+
+			delete item;
+			item = NULL;
+		}
+	}
+}
+
+void DefaultTreeModel::addNodeItem( TreeItem * node, TreeItem * nodeParent )
+{
+	node->setModel( dynamic_cast<Model*>(this) );
+	if ( NULL == nodeParent ){
+		rootNodes_.push_back( node );
+		TreeModelEvent event(dynamic_cast<Object*>(this), node, TreeModel::TREEITEM_ADDED );
+		RootNodeChanged.fireEvent( &event );
+	}
+	else {
+		nodeParent->addChild( node );
+	}
+
+	TreeModelEvent event(dynamic_cast<Object*>(this), node, TreeModel::TREEITEM_ADDED );
+	NodeAdded.fireEvent( &event );
+}
 
 /**
 *CVS Log info
 *$Log$
+*Revision 1.2.2.1  2004/10/05 03:15:23  kiklop74
+*Additional changes in tree model
+*
 *Revision 1.2  2004/08/07 02:49:07  ddiego
 *merged in the devmain-0-6-5 branch to stable
 *
