babfbedc0b56f7ac3358ae288834edce0f21bf35
marcelloptr <marcelloptr@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2004-12-20 23:48:07 +0000

    forbidden assignment operator for the Lock class

--- branches/devmain-0-6-7/vcf/src/vcf/FoundationKit/Lock.h
+++ branches/devmain-0-6-7/vcf/src/vcf/FoundationKit/Lock.h
@@ -40,6 +40,17 @@ public:
 protected:
 	bool validSynchObject_;
 	VCF::SynchObject& synchObject_;
+
+private:
+	/**
+	* we forbid to copy a lock, as it would probably
+	* cause some trouble.
+	* Plus it would be impossible to define an explicit 
+	* assignment operator with a reference. With this
+	* the CodeWarrior doesn't complain anymore.
+	*/
+	Lock& operator = ( const Lock& lock ) { return *this; };
+
 };
 } // end of namespace VCF
 
@@ -47,6 +58,9 @@ protected:
 /**
 *CVS Log info
 *$Log$
+*Revision 1.2.4.1  2004/12/20 23:48:07  marcelloptr
+*forbidden assignment operator for the Lock class
+*
 *Revision 1.2  2004/08/07 02:49:13  ddiego
 *merged in the devmain-0-6-5 branch to stable
 *
