13370a74746b654adbd9d20dfade3f699a80c57c
marcelloptr <marcelloptr@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2005-06-28 00:08:23 +0000

    improved Color class implementation

--- branches/devmain-0-6-7/vcf/build/vc60/add-ins/Autoexp/AutoExp.cpp
+++ branches/devmain-0-6-7/vcf/build/vc60/add-ins/Autoexp/AutoExp.cpp
@@ -1,4 +1,4 @@
- 
+
 
 //#include <windows.h>
 //#include "vcf/FoundationKit/FoundationKit.h"
@@ -84,7 +84,7 @@ HRESULT WINAPI VCF_DateTimeEvaluate( DWORD dwAddress, DEBUGHELPER *pHelper, int 
 
 		wsprintf( tmp, "%04lu-%02lu-%02lu %02lu:%02lu:%02lu.%04lu", year, month, day, hour, minute, second, millsecond );
 
-	} 
+	}
 	catch ( ... ) {
 		strcpy( tmp, "???*" );
 	}
@@ -117,7 +117,7 @@ HRESULT WINAPI VCF_DateTimeSpanEvaluate( DWORD dwAddress, DEBUGHELPER *pHelper, 
 
 		wsprintf( tmp, "%04lu-%02lu-%02lu %02lu:%02lu:%02lu.%04lu", year, month, day, hour, minute, second, millsecond );
 
-	} 
+	}
 	catch ( ... ) {
 		strcpy( tmp, "???*" );
 	}
@@ -178,7 +178,7 @@ HRESULT WINAPI VCF_ColorEvaluate( DWORD dwAddress, DEBUGHELPER *pHelper, int nBa
 		ColorSpace::RGBtype rgbType    = ColorSpace::ColorToRGB( color );
 		ColorSpace::RGBrangetype rgbRg = ColorSpace::RGBToRGBRange ( rgbType );
 		String rgbHex                  = Color::getHexCode( rgbRg.R, rgbRg.G, rgbRg.B, Color::cpsABGR );
-		uint32 rgb                     = color.getColorref32();
+		uint32 rgb                     = color.getColorRef32();
 
 		ColorSpace::HSLtype hslType    = ColorSpace::ColorToHSL( color );
 		ColorSpace::HSLrangetype hslRg = ColorSpace::HSLToHSLRange ( hslType );
@@ -189,16 +189,16 @@ HRESULT WINAPI VCF_ColorEvaluate( DWORD dwAddress, DEBUGHELPER *pHelper, int nBa
 			sprintf( tmp, "%-10s [ lum: %3d ]\n { RGB: [%3d,%3d,%3d] [%.2e,%.2e,%.2e] %s }\n { HSL: [%3d,%3d,%3d] [%.2e,%.2e,%.2e] %s }"
 									, name.ansi_c_str(), luminosity
 									, rgbRg.R, rgbRg.G, rgbRg.B, rgbType.R, rgbType.G, rgbType.B, rgbHex.ansi_c_str()
-									, hslRg.H, hslRg.S, hslRg.L, hslType.H, hslType.S, hslType.L, hslHex.ansi_c_str() 
+									, hslRg.H, hslRg.S, hslRg.L, hslType.H, hslType.S, hslType.L, hslHex.ansi_c_str()
 							);
 		} else {
 			sprintf( tmp, "%-10s [ lum: %3d ]\n { RGB: [%3d,%3d,%3d] [%.2lf,%.2lf,%.2lf] %s }\n { HSL: [%3d,%3d,%3d] [%.2lf,%.2lf,%.2lf] %s }"
 									, name.ansi_c_str(), luminosity
 									, rgbRg.R, rgbRg.G, rgbRg.B, rgbType.R, rgbType.G, rgbType.B, rgbHex.ansi_c_str()
-									, hslRg.H, hslRg.S, hslRg.L, hslType.H, hslType.S, hslType.L, hslHex.ansi_c_str() 
+									, hslRg.H, hslRg.S, hslRg.L, hslType.H, hslType.S, hslType.L, hslHex.ansi_c_str()
 							);
 		}
-	} 
+	}
 	catch ( ... ) {
 		strcpy( tmp, "??? !!" );
 	}
