0b4830df841b8422f552f9bf0a0952fa66fb7511
ddiego <ddiego@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2006-03-29 02:47:37 +0000

    updated vc projects with marcellos script.

--- branches/devmain-0-6-9/vcf/build/vc60/add-ins/COMToVCFClassWizard/COMTOVCFCLASSWIZARD.h
+++ branches/devmain-0-6-9/vcf/build/vc60/add-ins/COMToVCFClassWizard/COMTOVCFCLASSWIZARD.h
@@ -2,7 +2,7 @@
 
 
 /* File created by MIDL compiler version 5.01.0164 */
-/* at Sun Mar 26 23:03:51 2006
+/* at Mon Mar 27 21:48:11 2006
  */
 /* Compiler settings for f:\code\vcfdev\dev\vcf\build\vc60\add-ins\COMToVCFClassWizard\COMToVCFClassWizard.idl:
     Oicf (OptLev=i2), W1, Zp8, env=Win32, ms_ext, c_ext
