adbd622b150e02a46204559bbb2200128daa7d6f
ddiego <ddiego@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2006-03-29 03:06:55 +0000

    updated vc projects with marcellos script.

--- vcf/build/vc60/add-ins/VCFNewClassWiz/VCFNEWCLASSWIZ.h
+++ vcf/build/vc60/add-ins/VCFNewClassWiz/VCFNEWCLASSWIZ.h
@@ -2,7 +2,7 @@
 
 
 /* File created by MIDL compiler version 5.01.0164 */
-/* at Mon Mar 27 21:48:00 2006
+/* at Tue Mar 28 21:48:36 2006
  */
 /* Compiler settings for f:\code\vcfdev\dev\vcf\build\vc60\add-ins\VCFNewClassWiz\VCFNewClassWiz.idl:
     Oicf (OptLev=i2), W1, Zp8, env=Win32, ms_ext, c_ext
