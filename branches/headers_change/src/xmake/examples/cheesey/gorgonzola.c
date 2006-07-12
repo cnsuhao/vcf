8f7985071bc0cb970239f685b1f4a6132386fe4b
mispunt <mispunt@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2006-07-12 17:31:46 +0000

    Removed file/folder, because of problems with merging.

--- branches/headers_change/src/xmake/examples/cheesey/gorgonzola.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int gorgonzola()
-{
-	printf( "gorgonzola called!\n" );
-	return 0;
-}
--- branches/headers_change/src/xmake/examples/helloworld/test1.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "test1.h"
-
-int main( int argc, char** argv )
-{
-	printf( "Hello from %s!\n", __FILE__ );
-	return 0;
-}
