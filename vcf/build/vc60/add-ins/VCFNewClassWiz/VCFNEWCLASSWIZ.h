1847ead1b1d3c69c036d798a2f9f2df5c245771b
ddiego <ddiego@cccfda39-7811-0410-ae5f-bc4b32ab4073>
2008-06-22 02:47:47 +0000

    fixed some broken example projects.

--- vcf/build/vc60/add-ins/VCFNewClassWiz/VCFNEWCLASSWIZ.h
+++ vcf/build/vc60/add-ins/VCFNewClassWiz/VCFNEWCLASSWIZ.h
@@ -2,7 +2,7 @@
 
 
 /* File created by MIDL compiler version 5.01.0164 */
-/* at Mon Jun 16 21:19:13 2008
+/* at Mon Jun 16 23:01:53 2008
  */
 /* Compiler settings for f:\code\vcfdev\stable\vcf\build\vc60\add-ins\VCFNewClassWiz\VCFNewClassWiz.idl:
     Oicf (OptLev=i2), W1, Zp8, env=Win32, ms_ext, c_ext
